4fa81a6404879f5f18910063f2d1b7339216f50b
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2022-05-16 12:45:17 +0100

    config changes as list

--- dd_bandits/config_changes.py
+++ dd_bandits/config_changes.py
@@ -8,38 +8,46 @@ lr_types = ["mean_std_of_mean", "action_mean_of_std", "mean_mean_of_std"]
 eps_types = ["max_std_of_mean"]
 
 ADAPTIVE_CONFIG_CHANGES = {
-    f"lr_{lr_type}_{modulation}_eps_{eps_type}": {
-        "learning_rate": {"type": lr_type, "modulate": modulation},
-        "epsilon": {"type": eps_type},
-    }
+    f"lr_{lr_type}_{modulation}_eps_{eps_type}": [
+        {
+            "learning_rate": {"type": lr_type, "modulate": modulation},
+            "epsilon": {"type": eps_type},
+        }
+    ]
     for lr_type, modulation, eps_type in itertools.product(
         lr_types, lr_modulations, eps_types
     )
 }
 
 CONSTANT_LR_CONFIG_CHANGES = {
-    f"lr_constant_{value}_eps_{eps_type}": {
-        "learning_rate": {"type": "constant", "value": value},
-        "epsilon": {"type": eps_type},
-    }
+    f"lr_constant_{value}_eps_{eps_type}": [
+        {
+            "learning_rate": {"type": "constant", "value": value},
+            "epsilon": {"type": eps_type},
+        }
+    ]
     for value, eps_type in itertools.product(lr_constants, eps_types)
 }
 
 CONSTANT_EPS_CONFIG_CHANGES = {
-    f"lr_{lr_type}_{modulation}_eps_constant_{value}": {
-        "learning_rate": {"type": lr_type, "modulate": modulation},
-        "epsilon": {"type": "constant", "value": value},
-    }
+    f"lr_{lr_type}_{modulation}_eps_constant_{value}": [
+        {
+            "learning_rate": {"type": lr_type, "modulate": modulation},
+            "epsilon": {"type": "constant", "value": value},
+        }
+    ]
     for lr_type, modulation, value in itertools.product(
         lr_types, lr_modulations, eps_constants
     )
 }
 
 CONSTANT_CONFIG_CHANGES = {
-    f"lr_constant_{value_lr}_eps_constant_{value_eps}": {
-        "learning_rate": {"type": "constant", "value": value_lr},
-        "epsilon": {"type": "constant", "value": value_eps},
-    }
+    f"lr_constant_{value_lr}_eps_constant_{value_eps}": [
+        {
+            "learning_rate": {"type": "constant", "value": value_lr},
+            "epsilon": {"type": "constant", "value": value_eps},
+        }
+    ]
     for value_lr, value_eps in itertools.product(lr_constants, eps_constants)
 }
 
