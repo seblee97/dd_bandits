cffc5ece08c56390d153c30896234ae16a1f72e2
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2022-05-30 13:20:20 +0100

    sgd, rms, ucb ablation

--- dd_bandits/config_changes.py
+++ dd_bandits/config_changes.py
@@ -3,7 +3,8 @@ import itertools
 eps_constants = [0.1, 0.5, 0.9]
 lr_constants = [0.01, 0.1, 1]
 lr_modulations = [0.1, 0.05, 0.2]
-lr_types = ["mean_mean_of_std"]
+action_selections = ["ucb", "epsilon_greedy"]
+lr_types = ["mean_mean_of_std", "uncertainty_fraction", "linear_decay"]
 
 eps_types = [
     "mean_std_of_mean",
@@ -12,53 +13,47 @@ eps_types = [
     "mean_information_radius",
 ]
 
-ADAPTIVE_CONFIG_CHANGES = {
-    f"lr_{lr_type}_{modulation}_eps_{eps_type}": [
-        {
-            "learning_rate": {"type": lr_type, "modulate": {"factor": modulation}},
-            "epsilon": {"type": eps_type},
-        }
-    ]
-    for lr_type, modulation, eps_type in itertools.product(
-        lr_types, lr_modulations, eps_types
-    )
-}
-
-CONSTANT_LR_CONFIG_CHANGES = {
-    f"lr_constant_{value}_eps_{eps_type}": [
-        {
-            "learning_rate": {"type": "constant", "constant": {"value": value}},
-            "epsilon": {"type": eps_type},
-        }
-    ]
-    for value, eps_type in itertools.product(lr_constants, eps_types)
-}
-
-CONSTANT_EPS_CONFIG_CHANGES = {
-    f"lr_{lr_type}_{modulation}_eps_constant_{value}": [
-        {
-            "learning_rate": {"type": lr_type, "modulate": {"factor": modulation}},
-            "epsilon": {"type": "constant", "constant": {"value": value}},
-        }
-    ]
-    for lr_type, modulation, value in itertools.product(
-        lr_types, lr_modulations, eps_constants
-    )
-}
-
 CONSTANT_CONFIG_CHANGES = {
-    f"lr_constant_{value_lr}_eps_constant_{value_eps}": [
+    f"lr_constant_{value_lr}_eps_constant_{value_eps}_{action_selection}": [
         {
+            "optimiser": "sgd",
+            "action_selection": action_selection,
             "learning_rate": {"type": "constant", "constant": {"value": value_lr}},
             "epsilon": {"type": "constant", "constant": {"value": value_eps}},
         }
     ]
-    for value_lr, value_eps in itertools.product(lr_constants, eps_constants)
+    for value_lr, value_eps, action_selection in itertools.product(
+        lr_constants, eps_constants, action_selections
+    )
+}
+
+ADAPTIVE_SGD_CONFIG_CHANGES = {
+    f"lr_{lr_type}_{modulation}_eps_{eps_type}_{action_selection}": [
+        {
+            "optimiser": "sgd",
+            "action_selection": action_selection,
+            "learning_rate": {"type": lr_type, "modulate": {"factor": modulation}},
+            "epsilon": {"type": eps_type},
+        }
+    ]
+    for lr_type, modulation, eps_type, action_selection in itertools.product(
+        lr_types, lr_modulations, eps_types, action_selections
+    )
+}
+
+ADAPTIVE_RMS_CONFIG_CHANGES = {
+    f"rms_eps_{eps_type}_{action_selection}": [
+        {
+            "optimiser": "rms_prop",
+            "action_selection": action_selection,
+            "epsilon": {"type": eps_type},
+        }
+    ]
+    for eps_type, action_selection in itertools.product(eps_types, action_selections)
 }
 
 CONFIG_CHANGES = {
-    **ADAPTIVE_CONFIG_CHANGES,
-    **CONSTANT_LR_CONFIG_CHANGES,
-    **CONSTANT_EPS_CONFIG_CHANGES,
     **CONSTANT_CONFIG_CHANGES,
+    **ADAPTIVE_SGD_CONFIG_CHANGES,
+    **ADAPTIVE_RMS_CONFIG_CHANGES,
 }
