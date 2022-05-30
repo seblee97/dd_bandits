24a5ce84682b581561312e02a8ce34cd86ea4b5c
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2022-05-30 11:54:38 +0100

    implement adam and rms prop

--- dd_bandits/utils.py
+++ dd_bandits/utils.py
@@ -1,3 +1,4 @@
+import copy
 from typing import List, Union
 
 import numpy as np
@@ -5,10 +6,15 @@ import numpy as np
 
 class Estimator:
     def __init__(
-        self, mean_init: Union[float, int] = 0, std_init: Union[float, int] = 1
+        self,
+        optimiser,
+        mean_init: Union[float, int] = 0,
+        std_init: Union[float, int] = 1,
     ):
         self._mean_estimate = np.random.normal(mean_init, std_init / 10)
         self._std_estimate = std_init
+
+        self._optimiser = optimiser
 
     @property
     def mean_estimate(self):
@@ -19,18 +25,25 @@ class Estimator:
         return self._std_estimate
 
     def update(self, new_sample, learning_rate):
-        delta = new_sample - self._mean_estimate
-        self._mean_estimate += learning_rate * delta.mean()
+        delta = self._mean_estimate - new_sample
+        delta_bar = self._std_estimate**2 - delta**2
 
-        delta_bar = delta**2 - self._std_estimate**2
-        var = self._std_estimate**2 + learning_rate * delta_bar.mean()
+        update_step = self._optimiser.update(
+            gradient=np.array([delta.mean(), delta_bar.mean()]),
+            learning_rate=learning_rate,
+        )
+
+        self._mean_estimate += update_step[0]
+        var = self._std_estimate**2 + update_step[1]
         self._std_estimate = np.sqrt(var)
 
 
 class EstimatorGroup:
-    def __init__(self, total_num_estimators: int):
+    def __init__(self, total_num_estimators: int, optimiser):
         self._total_num_estimators = total_num_estimators
-        self._estimators = [Estimator() for _ in range(total_num_estimators)]
+        self._estimators = [
+            Estimator(copy.deepcopy(optimiser)) for _ in range(total_num_estimators)
+        ]
 
         self._mean_diffs = []
 
@@ -75,6 +88,61 @@ class EstimatorGroup:
     @property
     def mean_diffs(self):
         return self._mean_diffs
+
+
+class SGD:
+    def __init__(self):
+        pass
+
+    def update(self, gradient: np.ndarray, learning_rate):
+        return -learning_rate * gradient
+
+
+class RMSProp:
+    def __init__(
+        self,
+        eta: Union[float, int],
+        gamma: Union[float, int],
+        epsilon: Union[float, int],
+        dimension: int = 2,
+    ):
+        self._eta = eta
+        self._gamma = gamma
+        self._epsilon = epsilon
+
+        self._eg2 = np.zeros(dimension)
+
+    def update(self, gradient, learning_rate):
+        self._eg2 = self._gamma * self._eg2 + (1 - self._gamma) * gradient**2
+        return -self._eta * gradient / np.sqrt(self._eg2 + self._epsilon)
+
+
+class Adam:
+    def __init__(
+        self,
+        alpha: Union[float, int],
+        beta_1: Union[float, int],
+        beta_2: Union[float, int],
+        epsilon: Union[float, int],
+        dimension: int = 2,
+    ):
+        self._alpha = alpha
+        self._beta_1 = beta_1
+        self._beta_2 = beta_2
+        self._epsilon = epsilon
+
+        self._step_count = 0
+
+        self._m = np.zeros(dimension)
+        self._v = np.zeros(dimension)
+
+    def update(self, gradient, learning_rate):
+        self._step_count += 1
+        self._m = self._beta_1 * self._m + (1 - self._beta_1) * gradient
+        self._v = self._beta_2 * self._v + (1 - self._beta_2) * gradient**2
+        m_hat = self._m / (1 - self._beta_1**self._step_count)
+        v_hat = self._v / (1 - self._beta_2**self._step_count)
+        return -self._alpha * m_hat / (np.sqrt(v_hat) + self._epsilon)
 
 
 def sample_mean(rng, mean_range: List[Union[float, int]]):
