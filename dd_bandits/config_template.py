2d2b02df61aa6580f40cfcc60f7daf6e60caa272
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2022-05-30 11:53:40 +0100

    optimiser fgield with adam and rmsprop templates, linear decay lr template also

--- dd_bandits/config_template.py
+++ dd_bandits/config_template.py
@@ -25,6 +25,29 @@ class ConfigTemplate:
         dependent_variables_required_values=[[constants.CONSTANT]],
     )
 
+    _linear_decay_lr_template = config_template.Template(
+        fields=[
+            config_field.Field(
+                name=constants.INITIAL_LR,
+                types=[int, float],
+                requirements=[lambda x: x > 0],
+            ),
+            config_field.Field(
+                name=constants.FINAL_LR,
+                types=[int, float],
+                requirements=[lambda x: x > 0],
+            ),
+            config_field.Field(
+                name=constants.LR_DECAY,
+                types=[int, float],
+                requirements=[lambda x: x > 0],
+            ),
+        ],
+        level=[constants.LEARNING_RATE, constants.LINEAR_DECAY],
+        dependent_variables=[constants.LR_TYPE],
+        dependent_variables_required_values=[[constants.LINEAR_DECAY]],
+    )
+
     _modulate_mean_of_std_lr_template = config_template.Template(
         fields=[
             config_field.Field(
@@ -39,8 +62,60 @@ class ConfigTemplate:
                 constants.ACTION_MEAN_OF_STD,
                 constants.MEAN_MEAN_OF_STD,
                 constants.MEAN_STD_OF_MEAN,
+                constants.UNCERTAINTY_FRACTION,
             ]
         ],
+    )
+
+    _adam_template = config_template.Template(
+        fields=[
+            config_field.Field(
+                name=constants.ALPHA,
+                types=[int, float],
+                requirements=[lambda x: x > 0],
+            ),
+            config_field.Field(
+                name=constants.BETA_1,
+                types=[int, float],
+                requirements=[lambda x: x >= 0 and x <= 1],
+            ),
+            config_field.Field(
+                name=constants.BETA_2,
+                types=[int, float],
+                requirements=[lambda x: x >= 0 and x <= 1],
+            ),
+            config_field.Field(
+                name=constants.EPSILON,
+                types=[int, float],
+                requirements=[lambda x: x > 0],
+            ),
+        ],
+        level=[constants.ADAM],
+        dependent_variables=[constants.OPTIMISER],
+        dependent_variables_required_values=[[constants.ADAM]],
+    )
+
+    _rms_prop_template = config_template.Template(
+        fields=[
+            config_field.Field(
+                name=constants.ETA,
+                types=[int, float],
+                requirements=[lambda x: x > 0],
+            ),
+            config_field.Field(
+                name=constants.GAMMA,
+                types=[int, float],
+                requirements=[lambda x: x >= 0 and x <= 1],
+            ),
+            config_field.Field(
+                name=constants.EPSILON,
+                types=[int, float],
+                requirements=[lambda x: x > 0],
+            ),
+        ],
+        level=[constants.RMS_PROP],
+        dependent_variables=[constants.OPTIMISER],
+        dependent_variables_required_values=[[constants.RMS_PROP]],
     )
 
     _learning_rate_template = config_template.Template(
@@ -53,9 +128,11 @@ class ConfigTemplate:
                     lambda x: x
                     in [
                         constants.CONSTANT,
+                        constants.LINEAR_DECAY,
                         constants.ACTION_MEAN_OF_STD,
                         constants.MEAN_MEAN_OF_STD,
                         constants.MEAN_STD_OF_MEAN,
+                        constants.UNCERTAINTY_FRACTION,
                     ]
                 ],
             ),
@@ -68,6 +145,7 @@ class ConfigTemplate:
         level=[constants.LEARNING_RATE],
         nested_templates=[
             _constant_lr_template,
+            _linear_decay_lr_template,
             _modulate_mean_of_std_lr_template,
         ],
     )
@@ -86,6 +164,29 @@ class ConfigTemplate:
         dependent_variables_required_values=[[constants.CONSTANT]],
     )
 
+    _linear_decay_eps_template = config_template.Template(
+        fields=[
+            config_field.Field(
+                name=constants.INITIAL_EPS,
+                types=[int, float],
+                requirements=[lambda x: x >= 0 and x <= 1],
+            ),
+            config_field.Field(
+                name=constants.FINAL_EPS,
+                types=[int, float],
+                requirements=[lambda x: x >= 0 and x <= 1],
+            ),
+            config_field.Field(
+                name=constants.EPS_DECAY,
+                types=[int, float],
+                requirements=[lambda x: x > 0 and x <= 1],
+            ),
+        ],
+        level=[constants.EPSILON, constants.LINEAR_DECAY],
+        dependent_variables=[constants.EPS_TYPE],
+        dependent_variables_required_values=[[constants.LINEAR_DECAY]],
+    )
+
     _epsilon_template = config_template.Template(
         fields=[
             config_field.Field(
@@ -96,6 +197,7 @@ class ConfigTemplate:
                     lambda x: x
                     in [
                         constants.CONSTANT,
+                        constants.LINEAR_DECAY,
                         constants.MAX_STD_OF_MEAN,
                         constants.MEAN_STD_OF_MEAN,
                         constants.MEAN_AVERAGE_KL,
@@ -116,7 +218,7 @@ class ConfigTemplate:
             ),
         ],
         level=[constants.EPSILON],
-        nested_templates=[_constant_eps_template],
+        nested_templates=[_constant_eps_template, _linear_decay_eps_template],
     )
 
     _ucb_template = config_template.Template(
@@ -159,8 +261,18 @@ class ConfigTemplate:
                         in [constants.EPSILON_GREEDY, constants.UCB, constants.THOMPSON]
                     ],
                 ),
+                config_field.Field(
+                    name=constants.OPTIMISER,
+                    types=[str],
+                    requirements=[
+                        lambda x: x
+                        in [constants.SGD, constants.ADAM, constants.RMS_PROP]
+                    ],
+                ),
             ],
             nested_templates=[
+                self._adam_template,
+                self._rms_prop_template,
                 self._learning_rate_template,
                 self._epsilon_template,
                 self._ucb_template,
