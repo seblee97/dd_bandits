ef633d17686ed3b28aecc0b863baaeeb0c0d9b77
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2022-05-30 11:53:08 +0100

    base class action selection with children thompson, ucb and epsilon greedy

--- dd_bandits/action_selection.py
+++ dd_bandits/action_selection.py
@@ -1,18 +1,14 @@
+import abc
 from typing import Dict, List, Union
 
 import numpy as np
 from dd_bandits import constants
 
 
-class SelectAction:
+class SelectAction(abc.ABC):
     def __init__(self, config):
-        if config.action_selection == constants.EPSILON_GREEDY:
-            self._epsilon_computer = self._setup_epsilon_computer(config)
-        self._select_action = self._setup_action_selection(config)
-
         self._latest_metrics: Dict[str, np.ndarray]
         self._estimator_group: List
-        self._ucb_constant: Union[float, int]
         self._timestep: int = 0
 
         self._n_arms = config.n_arms
@@ -23,29 +19,28 @@ class SelectAction:
         self._latest_metrics = latest_metrics
         return self._select_action()
 
-    def _setup_action_selection(self, config):
-        if config.action_selection == constants.EPSILON_GREEDY:
-            return self._epsilon_greedy_action
-        elif config.action_selection == constants.UCB:
-            self._ucb_constant = config.ucb_constant
-            return self._ucb_action
-        elif config.action_selection == constants.THOMPSON:
-            return self._thompson_action
+    @abc.abstractmethod
+    def _select_action(self):
+        pass
 
-    def _epsilon_greedy_action(self):
-        epsilon = self._epsilon_computer()
 
-        # epsilon-greedy action selection
-        if np.random.random() < epsilon:
-            action = np.random.randint(self._n_arms)
-        else:
-            action = np.argmax(
-                [np.mean(eg.group_means) for eg in self._estimator_group]
-            )
+class ThompsonSampling(SelectAction):
+    def __init__(self, config):
+        super().__init__(config=config)
 
-        return action, {constants.EPSILON: epsilon}
+        self._alphas = np.zeros(self._n_arms)
+        self._betas = np.zeros(self._n_arms)
 
-    def _ucb_action(self):
+    def _select_action(self):
+        raise NotImplementedError
+
+
+class UCB(SelectAction):
+    def __init__(self, config):
+        super().__init__(config=config)
+        self._ucb_constant: Union[float, int] = config.ucb_constant
+
+    def _select_action(self):
         action_counts = self._latest_metrics[constants.ACTION_COUNTS]
         ucb_values = [
             np.mean(eg.group_means)
@@ -55,14 +50,26 @@ class SelectAction:
         action = np.argmax(ucb_values)
         return action, {}
 
-    def _thompson_action(self):
-        pass
+
+class EpsilonGreedy(SelectAction):
+    def __init__(self, config):
+        super().__init__(config=config)
+        self._epsilon_computer = self._setup_epsilon_computer(config)
 
     def _setup_epsilon_computer(self, config):
         if config.eps_type == constants.CONSTANT:
 
             def eps_fn():
                 return config.eps_value
+
+        elif config.eps_type == constants.LINEAR_DECAY:
+
+            initial_eps = config.initial_eps
+            final_eps = config.final_eps
+            eps_decay = config.eps_decay
+
+            def eps_fn():
+                return max([initial_eps - self._timestep * eps_decay, final_eps])
 
         elif config.eps_type == constants.MAX_STD_OF_MEAN:
 
@@ -105,3 +112,16 @@ class SelectAction:
                 return np.max([config.minimum_eps, computed_eps])
 
         return eps_fn
+
+    def _select_action(self):
+        epsilon = self._epsilon_computer()
+
+        # epsilon-greedy action selection
+        if np.random.random() < epsilon:
+            action = np.random.randint(self._n_arms)
+        else:
+            action = np.argmax(
+                [np.mean(eg.group_means) for eg in self._estimator_group]
+            )
+
+        return action, {constants.EPSILON: epsilon}
